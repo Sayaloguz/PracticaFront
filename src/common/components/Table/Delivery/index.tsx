9bf04929d2ba63794fc6883d39486ef5b4d84ff7
sara-lopez <sara.lopez@petroprix.com>
2025-04-23 13:48:52 +0200

    fix: [TABLE]: Adaptar a los DTO con información completa sin mostrar información de más

--- src/common/components/Table/Delivery/index.tsx
+++ src/common/components/Table/Delivery/index.tsx
@@ -2,6 +2,7 @@
 import { FC, useEffect, useState } from "react";
 import Service from "../../../../service/src/index";
 
+
 interface TableProps {
   useCase: string; // "getClients", "getMerchants"...
   endPointData?: Record<string, any>; // Datos adicionales opcionales
@@ -48,7 +49,27 @@ const Table: FC<TableProps> = ({ useCase, endPointData = {}, token = "" }) => {
   if (loading) return <p>Cargando...</p>;
   if (data.length === 0) return <p>No hay resultados</p>;
 
-  const columns = data.length > 0 ? Object.keys(data[0]) : [];
+
+  const excludedColumns = ["id", "status", "createTime"];
+  const columns = data.length > 0 
+    ? Object.keys(data[0]).filter((col) => !excludedColumns.includes(col)) 
+    : [];
+  
+
+    // TODO: Aprovechar que recorremos los datos para crear las URL de las acciones
+  const dataTable = data.map((row) => {
+    // Filtrar las claves excluidas directamente del objeto
+    const filteredRow = Object.keys(row)
+      .filter((key) => !excludedColumns.includes(key))
+      .reduce((acc, key) => {
+        acc[key] = row[key];
+        return acc;
+      }, {} as Record<string, any>); // Inicializar objeto vacío con tipado
+    return filteredRow;
+  });
+  
+  console.log("#### dataTable", dataTable);
+  
 
   return (
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
@@ -66,7 +87,7 @@ const Table: FC<TableProps> = ({ useCase, endPointData = {}, token = "" }) => {
           </tr>
         </thead>
         <tbody>
-          {data.map((row, index) => (
+          {dataTable.map((row, index) => (
             <tr
               key={index}
               className={`${
@@ -83,7 +104,7 @@ const Table: FC<TableProps> = ({ useCase, endPointData = {}, token = "" }) => {
               ))}
 
               { /* Acciones */ }
-              
+
               <td className="px-6 py-4">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6" >
                   <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
---
+++ src/common/components/Table/Infrastructure/tableFunctions.ts
@@ -0,0 +1,5 @@
+export const createPageUrl = (useCase: string) => {
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+  const pageUrl = `${baseUrl}/${useCase}`;
+  return pageUrl;
+}
