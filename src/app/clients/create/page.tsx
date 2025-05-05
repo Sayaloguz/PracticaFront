f57d233a3802839f472118b5c8c84cfb0a246cf6
sara-lopez <sara.lopez@petroprix.com>
2025-05-05 14:13:34 +0200

    build: [FORMULARIOS]: Componetización de los input

--- src/app/clients/create/page.tsx
+++ src/app/clients/create/page.tsx
@@ -1,12 +1,11 @@
 import ClientForm from "@/common/components/Forms/ClientForm/Delivery";
-import BreadCrumbs
 export default function UpdateClientPage() {
     return (
         <section>
             <h1 className="text-3xl md:text-4xl font-bold text-gray-900 dark:text-white">
                 Crear cliente
             </h1>
-
+            
             <ClientForm
                 action="createClient"
             />
